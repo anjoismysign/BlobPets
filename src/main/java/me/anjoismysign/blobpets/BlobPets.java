ca9ece7990afa6720738583e89ebe6433dbecb48
lbenav8095 <lubensaico@gmail.com>
2024-04-17 22:04:56 -0600

    Prepares the release

--- src/main/java/me/anjoismysign/blobpets/BlobPets.java
+++ src/main/java/me/anjoismysign/blobpets/BlobPets.java
@@ -1,15 +1,18 @@
 package me.anjoismysign.blobpets;
 
 import me.anjoismysign.blobpets.director.PetsManagerDirector;
+import us.mytheria.bloblib.entities.PluginUpdater;
 import us.mytheria.bloblib.managers.BlobPlugin;
 import us.mytheria.bloblib.managers.IManagerDirector;
 
 public final class BlobPets extends BlobPlugin {
+    private PluginUpdater updater;
     private IManagerDirector proxy;
     private BlobPetsAPI api;
 
     @Override
     public void onEnable() {
+        updater = generateGitHubUpdater("anjoismysign", "BlobPets");
         PetsManagerDirector director = new PetsManagerDirector(this);
         proxy = director.proxy();
         api = BlobPetsAPI.getInstance(director);
